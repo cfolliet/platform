275d717014b5102b1d5b63dee523db5fda9cf67d
Clément Folliet <cfolliet@talentsoft.com>
2020-02-13 17:21:47 +0100

    Draw in and out of the level

--- public/scripts/level.js
+++ public/scripts/level.js
@@ -3,7 +3,10 @@ import generateLevel from './levelGenerator.js'
 export default function createLevel(canvas, resources) {
     const tileWidth = 16;
     const tileHeight = 16;
-    const tiles = generateLevel(canvas.width, canvas.height);
+    const level = generateLevel(canvas.width, canvas.height);
+    const tiles = level.tiles;
+    const input = level.input;
+    const output = level.output;
     const background = resources.get('/images/background.jpg');
 
     function draw(context) {
@@ -17,6 +20,16 @@ export default function createLevel(canvas, resources) {
             context.strokeRect(x, y, w, h);
             context.drawImage(background, x, y, w, h, x, y, w, h);
         });
+
+        context.fillStyle = '#4f900d';
+        context.strokeStyle = '#f7ebeb';
+        context.fillRect(input.pos.x, input.pos.y, input.size.x, input.size.y)
+        context.strokeRect(input.pos.x, input.pos.y, input.size.x, input.size.y);
+        
+        context.fillStyle = '#890729';
+        context.strokeStyle = '#f7ebeb';
+        context.fillRect(output.pos.x, output.pos.y, output.size.x, output.size.y)
+        context.strokeRect(output.pos.x, output.pos.y, output.size.x, output.size.y);
     }
 
     return {
--- public/scripts/levelGenerator.js
+++ public/scripts/levelGenerator.js
@@ -1,20 +1,19 @@
-export default function generateLevel(width, height) {
-    const tileSize = 16;
+function roundToSize(n, size) {
+    return Math.ceil(n / size) * size;
+}
+
+function getTiles(width, height, tileSize) {
     const minBlocks = 15
     const maxBlocks = 50;
     const maxBlockWidth = 20 * tileSize;
     const maxBlockHeight = 5 * tileSize;
-    const level = []
+    const tiles = [];
 
-    level.push({ pos: { x: 0, y: 0 }, size: { x: width, y: tileSize * 2 } });
-    level.push({ pos: { x: 0, y: height - tileSize * 2 }, size: { x: width, y: tileSize * 2 } });
+    tiles.push({ pos: { x: 0, y: 0 }, size: { x: width, y: tileSize * 2 } });
+    tiles.push({ pos: { x: 0, y: height - tileSize * 2 }, size: { x: width, y: tileSize * 2 } });
 
-    level.push({ pos: { x: 0, y: 0 }, size: { x: tileSize * 2, y: height } });
-    level.push({ pos: { x: width - tileSize * 2, y: 0 }, size: { x: tileSize * 2, y: height } });
-
-    function roundToSize(n, size) {
-        return Math.ceil(n / size) * size;
-    }
+    tiles.push({ pos: { x: 0, y: 0 }, size: { x: tileSize * 2, y: height } });
+    tiles.push({ pos: { x: width - tileSize * 2, y: 0 }, size: { x: tileSize * 2, y: height } });
 
     const nbBlock = Math.max(minBlocks, Math.random() * maxBlocks | 0);
     for (let index = 0; index < nbBlock; index++) {
@@ -23,8 +22,57 @@ export default function generateLevel(width, height) {
         const w = Math.max(tileSize * 2, roundToSize(Math.random() * maxBlockWidth | 0, tileSize));
         const h = Math.max(tileSize * 2, roundToSize(Math.random() * maxBlockHeight | 0, tileSize));
 
-        level.push({ pos: { x: x, y: y }, size: { x: w, y: h } });
+        tiles.push({ pos: { x: x, y: y }, size: { x: w, y: h } });
     }
 
-    return level;
+    return tiles;
+}
+
+function getEmptyPosition(width, height, tiles, tileSize) {
+    let point = null;
+    let collide = false;
+
+    do {
+        point = {
+            pos: {
+                x: roundToSize(Math.random() * width | 0, tileSize),
+                y: roundToSize(Math.random() * height | 0, tileSize)
+            },
+            size: {
+                x: tileSize,
+                y: tileSize * 2
+            }
+        }
+
+        const posTop = point.pos.y;
+        const posBottom = point.pos.y + point.size.x;
+        const posLeft = point.pos.x;
+        const posRight = point.pos.x + point.size.y;
+
+        collide = tiles.some(tile => {
+            const tileTop = tile.pos.y;
+            const tileBottom = tile.pos.y + tile.size.y;
+            const tileLeft = tile.pos.x;
+            const tileRight = tile.pos.x + tile.size.x;
+
+            if (tileLeft < posRight && tileRight > posLeft &&
+                tileTop < posBottom && tileBottom > posTop) {
+                return true;
+            }
+            return false;
+        });
+    } while (collide);
+    return point;
+}
+
+export default function generateLevel(width, height, tileSize = 16) {
+    const tiles = getTiles(width, height, tileSize);
+    const input = getEmptyPosition(width, height, tiles, tileSize);
+    const output = getEmptyPosition(width, height, tiles, tileSize);
+
+    return {
+        tiles: tiles,
+        input: input,
+        output: output
+    };
 };
